c6f08e121926cc00f12e4e70d5d2cdd7d9589b47
Xingyao Wang <xingyaow@umich.edu>
2020-11-22 18:02:10 +0800

    修复GPT_qa相对路径问题

--- extensions/cpm_lm/sample.py
+++ extensions/cpm_lm/sample.py
@@ -1,14 +1,20 @@
 import tensorflow_hub as hub
 import tensorflow as tf
+import os
 
 from gpt2_tokenizer import GPT2Tokenizer
 
-tokenizer = GPT2Tokenizer(
-    'CPM-Generate/bpe_3w_new/vocab.json',
-    'CPM-Generate/bpe_3w_new/merges.txt',
-    model_file='CPM-Generate/bpe_3w_new/chinese_vocab.model')
+BASE_PATH = "/ErGo/extensions/cpm_lm"
+def process_relpath(relpath): return os.path.join(BASE_PATH, relpath)
 
-gpt = hub.load('./cpm-lm-tf2_v2/')
+
+tokenizer = GPT2Tokenizer(
+    process_relpath('CPM-Generate/bpe_3w_new/vocab.json'),
+    process_relpath('CPM-Generate/bpe_3w_new/merges.txt'),
+    model_file=process_relpath('CPM-Generate/bpe_3w_new/chinese_vocab.model')
+)
+
+gpt = hub.load(process_relpath('cpm-lm-tf2_v2/'))
 
 
 def sample(sentence, tokenizer=tokenizer, gpt=gpt, number=1, length=20, top_p=0.9, temperature=0.9):
@@ -31,8 +37,10 @@ def sample(sentence, tokenizer=tokenizer, gpt=gpt, number=1, length=20, top_p=0.
         for s in ret.numpy()
     ]
 
+
 if __name__ == "__main__":
-    ret = sample(tokenizer, gpt, '默写英文：\n狗dog\n猫cat\n鸟', 3, 10, top_p=0.9, temperature=0.9)
+    ret = sample(tokenizer, gpt, '默写英文：\n狗dog\n猫cat\n鸟',
+                 3, 10, top_p=0.9, temperature=0.9)
     for x in ret:
         print(x)
         print('-' * 20)
