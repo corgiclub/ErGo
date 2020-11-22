214f0616a6753f78fbc7da4a537d7733abfd1359
Xingyao Wang <xingyaow@umich.edu>
2020-11-22 19:34:11 +0800

    GPT-QA加入句尾截断

--- scripts/docker-start-bot.sh
+++ scripts/docker-start-bot.sh
@@ -2,4 +2,4 @@
 docker container stop ergo-bot-container
 docker container rm ergo-bot-container
 docker run -v /home/xingyaow/Projects/ErGo:/ErGo -itd --name ergo-bot-container ergo-bot /bin/bash
-docker exec ergo-bot-container sh -c "sh /ErGo/start-bot.sh"
+docker exec ergo-bot-container sh -c "sh /ErGo/scripts/start-bot.sh"
