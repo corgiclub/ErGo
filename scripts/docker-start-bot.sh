560aa1688637629019fcd4bb748c50f8525180bc
Xingyao Wang <xingyaow@umich.edu>
2020-11-22 18:01:25 +0800

    增加bot启动脚本

---
+++ scripts/docker-start-bot.sh
@@ -0,0 +1,5 @@
+# 启动docker容器并启动bot
+docker container stop ergo-bot-container
+docker container rm ergo-bot-container
+docker run -v /home/xingyaow/Projects/ErGo:/ErGo -itd --name ergo-bot-container ergo-bot /bin/bash
+docker exec ergo-bot-container sh -c "sh /ErGo/start-bot.sh"
