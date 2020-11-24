fae5ad5ea4fc774e575f76121cef96096793b742
LuneZ99 <lune_z@foxmail.com>
2020-11-24 11:11:18 +0800

    擦除显性config

--- extensions/load_config.py
+++ extensions/load_config.py
@@ -9,6 +9,11 @@ class Cfg:
 
 
 def load_config():
+    """
+    加载配置，要求文件名调用该函数的文件名与 config/ 文件夹下的配置文件相同。
+
+    :return: class Config
+    """
 
     name = os.path.basename(inspect.stack()[1][1])[:-3]
     config_json = json.load(open('config/'+name+'.json', 'r'))
