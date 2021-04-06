0d5916545f1e89dee2f2546e8aaeb459e28cd4ae
zhengmingzhang <zhengmingzhang@meituan.com>
2021-04-06 11:38:58 +0800

    test

--- plugins/noob.py
+++ plugins/noob.py
@@ -10,13 +10,13 @@ import re
 __plugin_name__ = '嘴臭'
 __plugin_description__ = '嘴臭bot'
 __plugin_usage__ = '发送"我叼你妈的"'
-__plugin_pattern__ = '我.*你妈的'
+__plugin_pattern__ = '重启vpn'
 
 bcc = Get.bcc()
 
 
 @bcc.receiver(GroupMessage, headless_decoraters=[judge.group_check(__name__)])
-async def video_info(app: GraiaMiraiApplication, group: Group, message: MessageChain, member: Member):
+async def noob(app: GraiaMiraiApplication, group: Group, message: MessageChain, member: Member):
     if re.match(__plugin_pattern__, message.asDisplay()):
         await app.sendGroupMessage(group, MessageChain.create([
             Plain(f"你妈死了"),
