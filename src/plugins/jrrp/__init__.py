b61dab6ec55b090da66756becdd158a583f45ea1
LuneZ99 <lune_z@foxmail.com>
2021-08-06 14:24:05 +0800

    feat<jrrp>: 随机数生成算法改为真随机
    jrrp 的随机数值生成算法改为
    通过 qrng.anu.edu.au 获得均匀分布真随机数 x_1, x_2
    通过 Box-Muller 变换转换为标准正态分布
    调整正态分布的参数并为其设置上下限
    返回jrrp

--- src/plugins/jrrp/__init__.py
+++ src/plugins/jrrp/__init__.py
@@ -1,4 +1,7 @@
+import httpx
+import os
 import nonebot
+import math
 
 from nonebot import on_command
 from nonebot.typing import T_State
@@ -16,24 +19,27 @@ jrrp = on_command('jrrp')
 
 @jrrp.handle()
 async def _(bot: Bot, event: Event, state: T_State):
-
     yml_path = "src/plugins/jrrp/jrrp.yml"
+
     if not event.is_tome():
         sender = event.get_user_id()
-
-        with open(yml_path, "r", encoding="utf-8") as f:
-            jrrp_log = yaml.load(f, Loader=yaml.FullLoader)
-
-        today = datetime.date.today()
-        if not jrrp_log['date'] == today:
+        if os.path.exists(yml_path):
+            with open(yml_path, 'r', encoding="utf-8") as f:
+                jrrp_log = yaml.load(f, Loader=yaml.FullLoader)
+            today = datetime.date.today()
+            if not jrrp_log['date'] == today:
+                jrrp_log = {
+                    'date': today
+                }
+        else:
             jrrp_log = {
-                'date': today
+                'date': datetime.date.today()
             }
 
         if sender in jrrp_log.keys():
             rp = jrrp_log[sender]
         else:
-            rp = randint(0, 100)
+            rp = await get_normal_random()
             jrrp_log[sender] = rp
             with open("src/plugins/jrrp/jrrp.yml", "w", encoding="utf-8") as f:
                 yaml.dump(jrrp_log, f)
@@ -54,3 +60,23 @@ async def _(bot: Bot, event: Event, state: T_State):
         ]
         await jrrp.send(msg)
 
+
+async def get_normal_random(mu=50, sig=25, limit=(0, 100)):
+
+    data_type = 'uint16'
+    data_one = 65536
+    array_length = 2
+
+    anu_api_url = f'https://qrng.anu.edu.au/API/jsonI.php?length={array_length}&type={data_type}'
+    resp = httpx.get(anu_api_url)
+
+    x1, x2 = resp.json()['data']    # x_1, x_2 \in U(0, 65536)
+    x1 /= data_one
+    x2 /= data_one
+    z = (-2 * math.log(x1)) ** 0.5 * math.cos(2 * math.pi * x2)
+    x = z * sig + mu
+
+    if limit[0] < x < limit[1]:
+        return round(x)
+    else:
+        return await get_normal_random(mu, sig, limit)
