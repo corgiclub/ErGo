e1d29c25a9d341b185569fd29c4228b511f2a82a
LuneZ99 <lune_z@foxmail.com>
2021-08-24 00:31:52 +0800

    feature: 聊天记录功能初步上线

--- src/extensions/mongodb.py
+++ src/extensions/mongodb.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
 from src.extensions.config import MongoDB
+from src.extensions.utils import PicSource
+from src.extensions.imghdr_byte import what
+import httpx
+import os
 
 
 cfg = MongoDB()
@@ -13,11 +17,27 @@ def test():
         'data': 'test'
     }
     return col.insert_one(data)
-    # print(result)
 
 
 def get_collection(db_name: str, col_name: str):
     return client[db_name][col_name]
 
 
-# test()
+async def log_picture(file: str, url: str, source: PicSource, base_pic_path: str = cfg.base_path + 'picture/'):
+    col = get_collection('picture', source)
+    if not col.find_one({"file": file}):
+        line = {
+            "file": file,
+        }
+        res = httpx.get(url)
+        if res.status_code == 200:
+            pic = res.content
+            suffix = what(pic)
+            path = f"{base_pic_path}{source}/{file}.{suffix}"
+            with open(path, 'wb') as fi:
+                fi.write(pic)
+            line["suffix"] = suffix
+        else:
+            line["failure"] = True
+
+        col.insert_one(line)
--- src/extensions/utils.py
+++ src/extensions/utils.py
@@ -1,4 +1,14 @@
+from enum import Enum
+
+
 def regex_equal(keywords) -> str:
     return '|'.join(('^'+k+'$' for k in keywords))
 
+
+class PicSource(str, Enum):
+    Chat = 'chat'
+    ChatRecord = 'chat_record'
+    Pixiv = 'pixiv'
+    Twitter = 'twitter'
+    SauceNAO = 'saucenao'
 
