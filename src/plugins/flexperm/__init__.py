347978840840fa11ee2dcf813c8c34745403e09a
LuneZ99 <lune_z@foxmail.com>
2021-10-19 03:21:14 +0800

    feature: 标准化权限及配置格式，将全插件权限暴露给其他插件

--- src/plugins/flexperm/__init__.py
+++ src/plugins/flexperm/__init__.py
@@ -1,9 +1,10 @@
 from nonebot.plugin.export import export
 
 from . import cmds
-from .plugin import register
+from .plugin import register, plugins
 from .cmds import reload
 
 export().register = register
+export().plugins_handle = plugins
 
 del export, cmds, register
