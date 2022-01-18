95d45a4b55c6b4815fc254aa73005c5fb672c7cc
Shimada666 <649940882@qq.com>
2022-01-19 01:29:45 +0800

    feat: db

--- bot/src/models/table.sql
+++ bot/src/models/table.sql
@@ -1,13 +1,15 @@
 create table chat
 (
-    id          bigint                                                            not null,
+    id          bigint auto_increment,
     group_id    bigint      default 0                                             not null comment '群 id',
     user_id     bigint      default 0                                             not null comment '用户 qq',
     message_id  int         default 0                                             null comment 'nonebot 内部消息ID',
     type        varchar(10) default ''                                            not null comment '消息段类型',
     add_time    datetime    default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime    default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int         default 0                                             not null comment '删除时间'
+    delete_time int         default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment '聊天表';
 
@@ -18,9 +20,11 @@ create table chat_text
     text        varchar(1023) default ''                                            not null comment 'text',
     add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    delete_time int           default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
-    comment '聊天表';
+    comment 'chat_text';
 
 create table chat_face
 (
@@ -29,7 +33,9 @@ create table chat_face
     face_id     bigint   default 0                                             not null comment 'QQ 表情 ID',
     add_time    datetime default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int      default 0                                             not null comment '删除时间'
+    delete_time int      default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_face';
 
@@ -42,7 +48,9 @@ create table chat_image
     url         varchar(1023) default ''                                            not null comment '图片链接',
     add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    delete_time int           default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_image';
 
@@ -55,7 +63,9 @@ create table chat_record
     file_name   varchar(1023) default ''                                            not null comment '音频文件名',
     add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    delete_time int           default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_record';
 
@@ -67,7 +77,9 @@ create table chat_video
     file_name   varchar(1023) default ''                                            not null comment '视频文件名',
     add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    delete_time int           default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_video';
 
@@ -78,7 +90,9 @@ create table chat_at
     qq          bigint   default 0                                             not null comment 'at 的 QQ 号（1 表示 at 全体）',
     add_time    datetime default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int      default 0                                             not null comment '删除时间'
+    delete_time int      default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_at';
 
@@ -92,7 +106,9 @@ create table chat_poke
     name        varchar(10) default ''                                            not null comment '戳一戳表情名',
     add_time    datetime    default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime    default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int         default 0                                             not null comment '删除时间'
+    delete_time int         default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_poke';
 
@@ -104,53 +120,61 @@ create table chat_share
     title       varchar(255)  default ''                                            not null comment '分享标题',
     add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    delete_time int           default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_share';
 
 create table chat_contact
 (
-    id          bigint                                                              not null,
-    chat_id     bigint        default 0                                             not null comment 'chat id',
-    type        tinyint(4)    default 0                                             not null comment '戳一戳类型',
-    contact_id  bigint        default 0                                             not null comment '被推荐人的 QQ 号 / 群号',
-    add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
-    update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    id          bigint                                                           not null,
+    chat_id     bigint     default 0                                             not null comment 'chat id',
+    type        tinyint(4) default 0                                             not null comment '戳一戳类型',
+    contact_id  bigint     default 0                                             not null comment '被推荐人的 QQ 号 / 群号',
+    add_time    datetime   default CURRENT_TIMESTAMP                             not null comment '添加时间',
+    update_time datetime   default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
+    delete_time int        default 0                                             not null comment '删除时间'
 )
     comment 'chat_contact';
 
 create table chat_location
 (
-    id          bigint                                                              not null,
-    chat_id     bigint        default 0                                             not null comment 'chat id',
-    lat         float         default 0                                             not null comment '纬度',
-    lot         float         default 0                                             not null comment '经度',
-    add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
-    update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    id          bigint                                                         not null,
+    chat_id     bigint   default 0                                             not null comment 'chat id',
+    lat         float    default 0                                             not null comment '纬度',
+    lot         float    default 0                                             not null comment '经度',
+    add_time    datetime default CURRENT_TIMESTAMP                             not null comment '添加时间',
+    update_time datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
+    delete_time int      default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_location';
 
 create table chat_reply
 (
-    id          bigint                                                              not null,
-    chat_id     bigint        default 0                                             not null comment 'chat id',
-    message_id  bigint        default 0                                             not null comment '回复消息时引用的 ID',
-    add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
-    update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    id          bigint                                                         not null,
+    chat_id     bigint   default 0                                             not null comment 'chat id',
+    message_id  bigint   default 0                                             not null comment '回复消息时引用的 ID',
+    add_time    datetime default CURRENT_TIMESTAMP                             not null comment '添加时间',
+    update_time datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
+    delete_time int      default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_reply';
 
 create table chat_forward
 (
-    id          bigint                                                              not null,
-    chat_id     bigint        default 0                                             not null comment 'chat id',
-    message_id  bigint        default 0                                             not null comment '合并转发 ID',
-    add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
-    update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    id          bigint                                                         not null,
+    chat_id     bigint   default 0                                             not null comment 'chat id',
+    message_id  bigint   default 0                                             not null comment '合并转发 ID',
+    add_time    datetime default CURRENT_TIMESTAMP                             not null comment '添加时间',
+    update_time datetime default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
+    delete_time int      default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_forward';
 
@@ -161,7 +185,9 @@ create table chat_xml
     data        varchar(1023) default ''                                            not null comment 'XML 内容',
     add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    delete_time int           default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_xml';
 
@@ -172,32 +198,36 @@ create table chat_json
     data        varchar(1023) default ''                                            not null comment 'JSON 内容',
     add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    delete_time int           default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'chat_json';
 
 create table image
 (
-    id          bigint                                                              not null,
-    filename    varchar(255)  default ''                                            not null comment '文件名',
-    suffix      tinyint(4)    default 0                                             not null comment '后缀名',
-    p_hash      binary(32)    default 0                                             not null comment '图片 P 哈希',
+    id          bigint                                                             not null,
+    filename    varchar(255) default ''                                            not null comment '文件名',
+    suffix      tinyint(4)   default 0                                             not null comment '后缀名',
+    p_hash      binary(32)   default 0                                             not null comment '图片 P 哈希',
 
-    add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
-    update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    add_time    datetime     default CURRENT_TIMESTAMP                             not null comment '添加时间',
+    update_time datetime     default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
+    delete_time int          default 0                                             not null comment '删除时间'
 )
     comment 'image';
 
 create table image_chat
 (
-    id          bigint                                                              not null,
-    image_id    bigint        default 0                                             not null comment 'image id',
-    qq_hash     varchar(32)   default ''                                            not null comment 'QQ 返回的 hash 值',
-    qq_count    bigint        default 0                                             not null comment '总计发送次数',
-    add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
-    update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    id          bigint                                                            not null,
+    image_id    bigint      default 0                                             not null comment 'image id',
+    qq_hash     varchar(32) default ''                                            not null comment 'QQ 返回的 hash 值',
+    qq_count    bigint      default 0                                             not null comment '总计发送次数',
+    add_time    datetime    default CURRENT_TIMESTAMP                             not null comment '添加时间',
+    update_time datetime    default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
+    delete_time int         default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'image_chat';
 
@@ -224,20 +254,24 @@ create table image_sauce
 
     add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
     update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    delete_time int           default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'image_sauce';
 
 create table image_tag
 (
-    id          bigint                                                              not null,
-    image_id    bigint        default 0                                             not null comment 'image id',
-    tag_source  tinyint(4)    default 0                                             not null comment 'tag 来源',
-    tag         varchar(255)  default ''                                            not null comment 'tag 内容',
+    id          bigint                                                             not null,
+    image_id    bigint       default 0                                             not null comment 'image id',
+    tag_source  tinyint(4)   default 0                                             not null comment 'tag 来源',
+    tag         varchar(255) default ''                                            not null comment 'tag 内容',
 
-    add_time    datetime      default CURRENT_TIMESTAMP                             not null comment '添加时间',
-    update_time datetime      default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
-    delete_time int           default 0                                             not null comment '删除时间'
+    add_time    datetime     default CURRENT_TIMESTAMP                             not null comment '添加时间',
+    update_time datetime     default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP not null comment '修改时间',
+    delete_time int          default 0                                             not null comment '删除时间',
+    constraint table_name_pk
+        primary key (id)
 )
     comment 'image_tag';
 
