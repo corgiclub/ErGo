d872ec72c7c7bbb7dd4f2124a35000b832b7f4e9
LuneZ99 <lune_z@foxmail.com>
2022-03-08 18:24:18 +0800

    feature: 数据库 models 更新

--- bot/src/models/chat.py
+++ bot/src/models/chat.py
@@ -1,4 +1,4 @@
-from peewee import BigIntegerField, CharField, IntegerField
+from peewee import BigIntegerField, CharField, IntegerField, SmallIntegerField, FloatField
 
 from src.models import db, BaseModel
 
@@ -7,13 +7,79 @@ class Chat(BaseModel):
     group_id = BigIntegerField()
     user_id = BigIntegerField()
     message_id = IntegerField()
-    type = CharField(max_length=10)
+    type = SmallIntegerField()
 
 
-class ChatText(BaseModel):
+class ChatBaseModel(BaseModel):
     chat_id = BigIntegerField(default=0, help_text='chat id')
+
+
+class ChatText(ChatBaseModel):
     text = CharField(max_length=1023, default='', help_text='纯文本内容')
 
 
+class ChatFace(ChatBaseModel):
+    face_id = BigIntegerField(default=0)
+
+
+class ChatImage(ChatBaseModel):
+    image_id = BigIntegerField(default=0)
+    qq_hash = CharField(max_length=32, default='', help_text='QQ 返回的hash值')
+    url = CharField(max_length=1023, default='', help_text='图片链接')
+
+
+class ChatRecord(ChatBaseModel):
+    file_url = CharField(max_length=1023, default='')
+    file_name = CharField(max_length=1023, default='')
+
+
+class ChatVideo(ChatBaseModel):
+    file_url = CharField(max_length=1023, default='')
+    file_name = CharField(max_length=1023, default='')
+
+
+class ChatAt(ChatBaseModel):
+    qq = BigIntegerField(default=0, help_text='at的qq号, 1表示全体成员')
+
+
+class ChatPoke(ChatBaseModel):
+    type = SmallIntegerField(default=0)
+    qq = BigIntegerField(default=0)
+    name = CharField(max_length=10, default='')
+
+
+class ChatShare(ChatBaseModel):
+    url = CharField(max_length=1023, default='')
+    title = CharField(max_length=255, default='')
+
+
+class ChatContact(ChatBaseModel):
+    type = SmallIntegerField(default=0)
+    contact_id = BigIntegerField(default=0)
+
+
+class ChatLocation(ChatBaseModel):
+    lat = FloatField(default=0)
+    lot = FloatField(default=0)
+
+
+class ChatReply(ChatBaseModel):
+    message_id = BigIntegerField(default=0)
+
+
+class ChatForward(ChatBaseModel):
+    message_id = BigIntegerField(default=0)
+
+
+class ChatXml(ChatBaseModel):
+    data = CharField(max_length=1023, default='')
+
+
+class ChatJson(ChatBaseModel):
+    data = CharField(max_length=1023, default='')
+
+
 if __name__ == '__main__':
-    print(Chat.get_by_id(2))
+
+    c = Chat.get(group_id=1234567, user_id=634493876, message_id=101, type=10)
+    c.delete_instance()
--- bot/src/models/image.py
+++ bot/src/models/image.py
@@ -1,19 +1,46 @@
-from peewee import BigIntegerField, CharField, IntegerField
+from peewee import BigIntegerField, BlobField, CharField, IntegerField, SmallIntegerField, FloatField
 
 from src.models import db, BaseModel
 
 
-class Chat(BaseModel):
-    group_id = BigIntegerField()
-    user_id = BigIntegerField()
-    message_id = IntegerField()
-    type = CharField(max_length=10)
+class Image(BaseModel):
+    filename = CharField(max_length=255, default='')
+    suffix = SmallIntegerField(default=0)
+    p_hash = BigIntegerField()
 
 
-class ChatText(BaseModel):
-    chat_id = BigIntegerField(default=0, help_text='chat id')
-    text = CharField(max_length=1023, default='', help_text='纯文本内容')
+class ImageChat(BaseModel):
+    image_id = BigIntegerField(default=0)
+
+    qq_hash = CharField(max_length=32, default='')
+    qq_count = BigIntegerField(default=0)
+
+
+class ImageSauce(BaseModel):
+    image_id = BigIntegerField(default=0)
+
+    thumbnail = CharField(max_length=1023, default='')
+    similarity = CharField(max_length=1023, default='')
+    index_id = CharField(max_length=1023, default='')
+    index_name = CharField(max_length=1023, default='')
+    title = CharField(max_length=1023, default='')
+    urls = CharField(max_length=1023, default='')
+    author = CharField(max_length=1023, default='')
+    raw = CharField(max_length=1023, default='')
+
+    pixiv_id = BigIntegerField(default=0)
+    twitter_id = CharField(max_length=1023, default='')
+
+    part = BigIntegerField(default=0)
+    year = BigIntegerField(default=0)
+    est_time = CharField(max_length=1023, default='')
+
+
+class ImageTag(BaseModel):
+    image_id = BigIntegerField(default=0, help_text='chat id')
+    tag_source = SmallIntegerField(default=0)
+    tag = CharField(max_length=255, default='')
 
 
 if __name__ == '__main__':
-    print(Chat.get_by_id(2))
+    print(ImageSauce.get_by_id(2))
